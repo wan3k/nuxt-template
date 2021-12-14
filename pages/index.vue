c1f73f4cea8ac848b014cd044e277cd5536e6f4c
icing545 <icing545@gmsil.com>
2021-12-14 15:09:57 -0800

    Updated name and message.

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
     <div class='mt-20'>
       <Logo style='width:120px;margin:auto' />
       <h1 class='text-center text-3xl mt-10'>
-        NuxtJS KickStart Template
+        Siri Lee
       </h1>
       <h2 class='text-center'>
-        Create simple, beautiful, and elegant website.
+        Happy Everyday.
       </h2>
       
     </div>
